c9008371505a45a2f17a7a57d9dbaa8c3b6c42b9
Marcelo Carvalho <49615369+marcelocarvalhoqueiroz@users.noreply.github.com>
2021-10-24 17:32:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'marcelocarvalhoqueiroz',
+  youtube: 'channel/UCLti1wEtefvt25_XlAnNtUQ',
+  facebook: 'marcelocarvalho',
+  instagram: 'marcelocqin',
+  twitter: 'marcelocarvalho'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialMedia.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitProfileInfo() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  // o promisse é a partir do .then
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitProfileInfo()
